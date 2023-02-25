98eb641cdaaefa8bcf8e91c4592bd58772df03a9
Wahid Sakib <wsakib87@gmail.com>
2023-02-26 00:46:39 +0600

    Added server started port and log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
+const PORT = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  Logger.log(
+    `Server started at http://localhost:${PORT}`,
+    `NestApplicationPORT`,
+  );
 }
 bootstrap();
