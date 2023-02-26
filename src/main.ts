75e5b5b18b51cf09c6628e75b8f8abba65dfae13
Wahid Sakib <wsakib87@gmail.com>
2023-02-26 11:44:02 +0600

    added database connection

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
-const PORT = 3000;
 async function bootstrap() {
+  const { PORT = 3001 } = process.env;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
-  Logger.log(
-    `Server started at http://localhost:${PORT}`,
-    `NestApplicationPORT`,
-  );
+  Logger.log(`Server started at http://localhost:${PORT}`, `NestApplication`);
 }
 bootstrap();
